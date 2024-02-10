fdc81fc7b7e2c4ce0fddf03a7d98c7cad0d1ff15
usiqwerty <benlogin1@protonmail.com>
2024-02-10 11:04:03 +0500

    Python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+	name='micro-whois',
+	version='1.0',
+	packages=find_packages(),
+	url='https://github.com/usiqwerty/micro-whois',
+	license='MIT License',
+	author='usiqwerty',
+	author_email='',
+	description='Cross-platform whois utility in pure Python'
+)
